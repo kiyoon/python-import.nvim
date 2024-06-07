456eb9c205aaa8992d1615d57168bbdb7c779f1f
Kiyoon Kim <kiyoon@users.noreply.github.com>
2024-06-07 23:58:21 +0900

    .

--- lua/python_import/ts_utils.lua
+++ lua/python_import/ts_utils.lua
@@ -34,74 +34,15 @@ local M = {}
 ---@param ignore_injections? boolean defaults true
 ---@return TSNode|nil
 function M.get_node_at_cursor(winnr, ignore_injections)
-	winnr = winnr or 0
-	local cursor = vim.api.nvim_win_get_cursor(winnr) -- line: 1-indexed, col: 0-indexed
-	local insert_offset = ((winnr == 0 or winnr == vim.api.nvim_get_current_win()) and vim.fn.mode() == "i") and 1 or 0
+  winnr = winnr or 0
+  local cursor = vim.api.nvim_win_get_cursor(winnr) -- line: 1-indexed, col: 0-indexed
+  local insert_offset = ((winnr == 0 or winnr == vim.api.nvim_get_current_win()) and vim.fn.mode() == "i") and 1 or 0
 
-	-- Treesitter: row, col are both 0-indexed
-	local cursor_pos = { cursor[1] - 1, cursor[2] - insert_offset }
-	assert(vim.treesitter.get_node, "nvim < 0.9 is unsupported.")
+  -- Treesitter: row, col are both 0-indexed
+  local cursor_pos = { cursor[1] - 1, cursor[2] - insert_offset }
+  assert(vim.treesitter.get_node, "nvim < 0.9 is unsupported.")
 
-	return vim.treesitter.get_node({ pos = cursor_pos, ignore_injections = ignore_injections })
+  return vim.treesitter.get_node { pos = cursor_pos, ignore_injections = ignore_injections }
 end
 
----@backport nvim-treesitter.ts_utils.get_root_for_position()
---- (only for compatibility during migration to vim.treesitter APIs, 0.9.0+)
----@deprecated
-function M.get_root_for_position(line, col, root_lang_tree)
-	local parsers = require("nvim-treesitter.parsers")
-
-	if not root_lang_tree then
-		if not parsers.has_parser() then
-			return
-		end
-
-		root_lang_tree = parsers.get_parser()
-	end
-
-	local lang_tree = root_lang_tree:language_for_range({ line, col, line, col })
-
-	for _, tree in pairs(lang_tree:trees()) do
-		local root = tree:root()
-
-		if root and vim.treesitter.is_in_node_range(root, line, col) then
-			return root, tree, lang_tree
-		end
-	end
-
-	-- This isn't a likely scenario, since the position must belong to a tree somewhere.
-	return nil, nil, lang_tree
-end
-
----@backport nvim-treesitter.ts_utils.get_vim_range()
--- Get a compatible vim range (1 index based) from a TS node range.
---
--- TS nodes start with 0 and the end col is ending exclusive.
--- They also treat a EOF/EOL char as a char ending in the first
--- col of the next row.
----@param range integer[]
----@param buf integer|nil
----@return integer, integer, integer, integer
----@deprecated
-function M.get_vim_range(range, buf)
-	---@type integer, integer, integer, integer
-	local srow, scol, erow, ecol = unpack(range)
-	srow = srow + 1
-	scol = scol + 1
-	erow = erow + 1
-
-	if ecol == 0 then
-		-- Use the value of the last col of the previous row instead.
-		erow = erow - 1
-		if not buf or buf == 0 then
-			ecol = vim.fn.col({ erow, "$" }) - 1
-		else
-			ecol = #vim.api.nvim_buf_get_lines(buf, erow - 1, erow, false)[1]
-		end
-		ecol = math.max(ecol, 1)
-	end
-	return srow, scol, erow, ecol
-end
-
-_G.ts_utils = M
 return M
