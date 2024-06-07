d353755079141fab2cd30f1b4b01754d7e1d6075
Kiyoon Kim <kiyoon@users.noreply.github.com>
2024-06-08 00:54:43 +0900

    .

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import versioneer
 from setuptools import setup
 
--- src/python_import/__init__.py
+++ src/python_import/__init__.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from . import _version
 
 __version__ = _version.get_versions()["version"]
