1d6cc9a40335bd4cd00a302ae3d59cbbaf2bd577
Kiyoon Kim <kiyoon@users.noreply.github.com>
2024-08-26 19:18:05 +0900

    .

--- src/python_import/cli/main.py
+++ src/python_import/cli/main.py
@@ -11,6 +11,7 @@ import typer
 from tree_sitter import Language, Parser
 
 import python_import
+from python_import.ts_utils import get_node
 from python_import.utils import get_all_imports_in_file_as_absolute
 
 PY_LANGUAGE = Language(tspython.language())
@@ -30,6 +31,46 @@ def common(
     version: bool = typer.Option(None, "-v", "--version", callback=version_callback),
 ):
     pass
+
+
+def find_matches(project_root: str, keyword: str) -> dict[str, list[tuple[int, int]]]:
+    # NOTE: rg json outputs are (1, 0)-indexed
+    rg_outputs = subprocess.run(
+        [
+            "fd",
+            "-e",
+            "py",
+            "-x",
+            "rg",
+            "--word-regexp",
+            "--fixed-strings",
+            "--json",
+            keyword,
+        ],
+        cwd=project_root,
+        capture_output=True,
+    )
+    # print(rg_outputs)
+
+    # 0-indexed row, col
+    file_path_to_rowcol: dict[str, list[tuple[int, int]]] = defaultdict(list)
+    for line in rg_outputs.stdout.decode("utf-8").split("\n"):
+        if not line:
+            continue
+        # print(line)
+        rg_output = json.loads(line)
+        # print(rg_output["type"])
+        if rg_output["type"] == "match":
+            file_path = str(
+                (Path(project_root) / rg_output["data"]["path"]["text"]).resolve()
+            )
+            row = rg_output["data"]["line_number"] - 1
+            col = rg_output["data"]["submatches"][0]["start"]
+            # col_end = rg_output["data"]["submatches"][0]["end"]
+            file_path_to_rowcol[file_path].append((row, col))
+
+    # print(file_path_to_rowcol)
+    return file_path_to_rowcol
 
 
 @app.command()
@@ -60,43 +101,7 @@ def count(
     """
     parser = Parser(PY_LANGUAGE)
 
-    # NOTE: rg json outputs are (1, 0)-indexed
-    rg_outputs = subprocess.run(
-        [
-            "fd",
-            "-e",
-            "py",
-            "-x",
-            "rg",
-            "--word-regexp",
-            "--fixed-strings",
-            "--json",
-            module_name,
-        ],
-        cwd=project_root,
-        capture_output=True,
-    )
-    # print(rg_outputs)
-
-    # 0-indexed row, col
-    file_path_to_rowcol: dict[str, list[tuple[int, int]]] = defaultdict(list)
-    for line in rg_outputs.stdout.decode("utf-8").split("\n"):
-        if not line:
-            continue
-        # print(line)
-        rg_output = json.loads(line)
-        # print(rg_output["type"])
-        if rg_output["type"] == "match":
-            file_path = str(
-                (Path(project_root) / rg_output["data"]["path"]["text"]).resolve()
-            )
-            row = rg_output["data"]["line_number"] - 1
-            col = rg_output["data"]["submatches"][0]["start"]
-            # col_end = rg_output["data"]["submatches"][0]["end"]
-            file_path_to_rowcol[file_path].append((row, col))
-
-    # print(file_path_to_rowcol)
-
+    file_path_to_rowcol = find_matches(project_root, module_name)
     import_statement_to_count: dict[str, int] = defaultdict(int)
 
     for python_file_path, rowcols in file_path_to_rowcol.items():
@@ -126,8 +131,30 @@ def count(
 
 
 @app.command()
-def dummy_do_not_use() -> None:
-    print(count.__doc__)
+def from_definition(
+    project_root: str,
+    name_to_find: str,
+) -> None:
+    """
+    Find all definitions of a module in a project and print like `from found_module import name_to_find`.
+
+    For example, it will find `def name_to_find():` or `class name_to_find:` or `name_to_find = 123`.
+    """
+    parser = Parser(PY_LANGUAGE)
+
+    file_path_to_rowcol = find_matches(project_root, name_to_find)
+    for python_file_path, rowcols in file_path_to_rowcol.items():
+        # find `def name_to_find():` but it has to be module-level. Decorators may be present.
+        with open(python_file_path) as f:
+            lines: str = f.read()
+
+        tree = parser.parse(bytes(lines, "utf8"))
+
+        for rowcol in rowcols:
+            node = get_node(tree, rowcol)
+            if node.type == "identifier":
+                print(node.parent)
+                print(node.parent.parent.text)
 
 
 def version():
