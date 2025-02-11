62b242009530182b9444bd72824c382e77381fd6
Kiyoon Kim <kiyoon@users.noreply.github.com>
2025-02-11 14:43:45 +0900

    .

--- src/python_import/__init__.py
+++ src/python_import/__init__.py
@@ -1,9 +1,5 @@
+from __future__ import annotations
+
 from ._version import get_version_dict
 
 __version__ = get_version_dict()["version"]
-
-from __future__ import annotations
-
-from . import _version
-
-__version__ = _version.get_versions()["version"]
